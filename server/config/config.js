f0d71ae6f5d394d2d41fd291202a4459abaa3e66
yanlinnn15 <yanlinnn155@gmail.com>
2025-07-30 17:27:17 +0800

    update db

--- server/config/config.js
+++ server/config/config.js
@@ -16,11 +16,7 @@ module.exports = {
     dialect: "mysql"
   },
   production: {
-    username: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    host: process.env.MYSQLHOST,
-    port: process.env.MYSQLPORT,
+    url: process.env.MYSQL_URL,
     dialect: "mysql"
   }
 };
