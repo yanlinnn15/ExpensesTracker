a99d757782182d5a8094a8b9117d532ae602b710
yanlinnn15 <yanlinnn155@gmail.com>
2025-07-30 17:14:30 +0800

    Update database configuration to use environment variables

---
+++ server/config/config.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: "root",
+    password: "0000",
+    database: "petdb",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  production: {
+    username: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    dialect: "mysql"
+  }
+};
