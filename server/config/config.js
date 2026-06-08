5a9041b97ed0dd030536bd6bb6a7843f21cb38bf
yanlinnn15 <yanlinnn155@gmail.com>
2026-06-08 15:01:02 +0800

    change some config

--- server/config/config.js
+++ server/config/config.js
@@ -18,13 +18,9 @@ module.exports = {
         dialect:  process.env.DB_DIALECT,
     },
     production: {
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-        host:     process.env.DB_HOST,
-        port:     process.env.DB_PORT || 5432,
-        dialect:  process.env.DB_DIALECT,
-        logging:  false,
+        url:     process.env.DB_URL,
+        dialect: process.env.DB_DIALECT,
+        logging: false,
         dialectOptions: {
             ssl: {
                 require: true,
